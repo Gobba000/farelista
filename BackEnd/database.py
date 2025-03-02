d4c12a2b181d34e893ddc0ef4a393c7064ee2c6d
Alex <alexgobbato.ag@gmail.com>
2025-03-02 10:13:02 +0100

    aggiunta ignora e app .py

---
+++ BackEnd/database.py
@@ -0,0 +1,18 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+# from models import *
+
+cred=credentials.Certificate("./progetto01/ServiceAccountKey.json")
+app=firebase_admin.initialize_app(cred)
+db=firestore.client(app)
+
+def add_prodotto(nome:str):
+    db.collection("prodotti").document(nome).set({"nome":nome})
+
+def get_prodotti():
+    prodotti=[]
+    for prodotto in db.collection("prodotti").stream():
+        prodotti+=[prodotto.to_dict()]
+        # prodotti.append(prodotti.to_dict())
+    return prodotti
